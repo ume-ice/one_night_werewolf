59986ddebeb18268acd4a293486f96ae3a8a0b45
ume-ice <syou0216@gmail.com>
2020-11-17 00:10:47 +0900

    add basic function

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,25 @@
           ゲームマスター <div class="help-box">？</div>
         </div>
         <div class="form-content">
-          <div style="margin-bottom: 20px;">
-            <label><input type="radio" name="mode" v-model="mode" :value="true" disabled>あり (comming soon)</label>
+          <div>
+            <label><input type="radio" v-model="mode" :value="true" disabled>あり (comming soon)</label>
           </div>
           <div>
-            <label><input type="radio" name="mode" v-model="mode" :value="false">なし</label>
+            <label><input type="radio" v-model="mode" :value="false">なし</label>
+          </div>
+        </div>
+      </div>
+
+      <div class="form-group">
+        <div class="form-title">
+          投票方法 <div class="help-box">？</div>
+        </div>
+        <div class="form-content">
+          <div>
+            <label><input type="radio" v-model="isVoteInTurn" :value="true" disabled>順番に投票 (comming soon)</label>
+          </div>
+          <div>
+            <label><input type="radio" v-model="isVoteInTurn" :value="false">追放する人を選択</label>
           </div>
         </div>
       </div>
@@ -76,11 +90,12 @@
           </div>
           <a v-show="!isShowJob" href="#" @click.prevent.stop="toggleIsShowJob(true)">役職を見る</a>
           <div v-show="isShowJob">
-            あなたの役職は{{getJobName(activePlayer.job)}}です。
+            あなたの役職は<span class="strong-text">{{getJobName(activePlayer.job)}}</span>です。
 
             <div v-show="activePlayer.job === 'fortune'" class="margin-top">
               <div v-if="isShowFotunedPlayer">
-                {{getPlayerName(fortunePlayerIndex)}}さんは{{getJobName(fortunedPlayer.job)}}です。
+                <span v-if="fortunePlayerIndex === 'center'">余ったカードは<span class="strong-text">{{getJobName(emptyJobs[0])}}</span>と<span class="strong-text">{{getJobName(emptyJobs[1])}}</span>です</span>
+                <span v-else><span class="strong-text">{{getPlayerName(fortunePlayerIndex)}}さん</span>は<span class="strong-text">{{getJobName(fortunedPlayer.job)}}</span>です。</span>
               </div>
               <div v-else>
                 占う人を選択してください。
@@ -92,6 +107,7 @@
                     :key="index"
                     :value="index"
                     :disabled="index == playerIndex">{{getPlayerName(index)}}</option>
+                  <option value="center">余ったカード</option>
                 </select>
                 <br>
                 <a href="#" @click.prevent.stop="fortunePlayer(fortunePlayerIndex)">占う</a>
@@ -100,7 +116,7 @@
 
             <div v-show="activePlayer.job === 'thief'" class="margin-top">
               <div v-if="isChanged">
-                あなたは{{getJobName(activePlayer.finalJob)}}になりました。
+                <span class="strong-text">{{getPlayerName(changePlayerIndex)}}</span>さんと交換して<span class="strong-text">{{getJobName(activePlayer.finalJob)}}</span>になりました。
               </div>
               <div v-else>
                 交換する人を選択してください。
@@ -118,16 +134,72 @@
               </div>
             </div>
 
+            <div v-show="activePlayer.job === 'werewolf'" class="margin-top">
+              <span v-if="Number.isInteger(otherWereWolf)"><span class="strong-text">{{getPlayerName(otherWereWolf)}}さん</span>も<span class="strong-text">人狼</span>です。</span>
+              <span v-else>他に<span class="strong-text">人狼のプレイヤーはいません</span></span>
+            </div>
+
             <div v-show="isShowJob && (activePlayer.job !== 'fortune' || isShowFotunedPlayer)">
               「次へ」を押してください
             </div>
 
           </div>
 
-        <div>
-          ------------------------------------
-          <div v-for="(player,index) in playerList" :key="index">{{getPlayerName(index)}}, {{getJobName(player.job)}} {{player.finalJob ? ' => ' + getJobName(player.finalJob) : ''}}</div>
         </div>
+      </div>
+    </div>
+
+    <div v-show="activePage.page === 'talk'">
+      <div class="form-group">
+        <div class="form-title">
+          話し合い
+        </div>
+        <div class="form-content">
+          夜が明けましたね。
+          <br>
+          話し合いをスタートしましょう。
+          <br>
+          まだタイマーはないので終わったら次へを押してね。
+        </div>
+      </div>
+    </div>
+
+    <div v-show="activePage.page === 'vote'">
+      <div class="form-group">
+        <div class="form-title">
+          投票
+        </div>
+        <div class="form-content">
+          追放する人を選択してください。
+          <div v-for="(player, index) in playerList" :key="index">
+            <label>
+              <input type="checkbox" v-model="player.isVote">{{getPlayerName(index)}}
+            </label>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div v-show="activePage.page === 'result'">
+      <div class="form-group">
+        <div class="form-title">
+          結果
+        </div>
+        <div class="form-content">
+          {{executionPlayerName}}を追放しました。
+          <div class="margin-top">
+            {{winnerTeam === 'werewolf' ? '人狼' : '村人'}}チームの勝利です。
+          </div>
+
+          <div class="margin-top">
+            <div v-for="(player,index) in playerList" :key="index">
+              <span v-if="getPlayerResult(player.finalJob || player.job)" class="strong-text">勝ち</span>
+              <span v-else class="strong-text" style="color: blue;">負け</span>
+              {{getPlayerName(index)}},
+              {{getJobName(player.job)}}
+              {{player.finalJob ? '=> ' + getJobName(player.finalJob) : ''}}
+            </div>
+          </div>
 
         </div>
       </div>
@@ -137,7 +209,7 @@
 
   <div class="footer">
     <div class="next-button" @click="moveTopPage">トップ</div>
-    <div class="next-button" @click="nextPage">{{buttonText}}</div>
+    <div v-show="activePage.page !== 'result'" class="next-button" @click="nextPage">{{buttonText}}</div>
   </div>
 </template>
 
@@ -148,19 +220,18 @@ export default {
       buttonText: '次へ',
       page: 1,
       mode: false,
+      isVoteInTurn: false,
       pageObj: {
         1: {page: 'mode', name: ''},
         2: {page: 'player', name: 'プレイヤー'},
         3: {page: 'job', name: '役職'},
         4: {page: 'night-start', name: '夜のターン'},
         5: {page: 'night-turn', name: '役職確認'},
+        6: {page: 'talk', name: '話し合い'},
+        7: {page: 'vote', name: '投票'},
+        8: {page: 'result', name: '結果'},
       },
-      playerList: [
-        {name: '', job:'', finalJob:''},
-        {name: '', job:'', finalJob:''},
-        {name: '', job:'', finalJob:''},
-        {name: '', job:'', finalJob:''},
-      ],
+      playerList: [],
       emptyJobs: [],
       jobs: [
         'werewolf',
@@ -180,19 +251,64 @@ export default {
       isChanged: false,
     }
   },
+  mounted () {
+    for (let i =0; i < 4; i++) {
+      this.addPlayer()
+    }
+  },
   computed: {
     activePage () {
       return this.pageObj[this.page]
     },
     activePlayer () {
-      return this.playerList[this.playerIndex]
+      return this.playerList[this.playerIndex] || {}
     },
     fortunedPlayer () {
       return this.playerList[this.fortunePlayerIndex] || {}
+    },
+    otherWereWolf () {
+      if (this.activePlayer.job !== 'werewolf') {
+        return null
+      }
+      let final = null
+      this.playerList.forEach((player, index) => {
+        if (player.job === 'werewolf' && index != this.playerIndex) {
+          final = index
+        }
+      })
+      return final
+    },
+    executionPlayerIndexes () {
+      let final = []
+      this.playerList.forEach((player, index) => {
+        if (player.isVote) {
+          final.push(index)
+        }
+      })
+      return final
+    },
+    executionPlayerName () {
+      return this.executionPlayerIndexes.map((playerIndex) => {
+        return this.getPlayerName(playerIndex) + 'さん'
+      }).join(',')
+    },
+    winnerTeam () {
+      let winner = 'werewolf'
+      let player
+      this.executionPlayerIndexes.forEach((playerIndex) => {
+        player = this.playerList[playerIndex]
+        if ((player.finalJob || player.job) === 'werewolf') {
+          winner = 'villager'
+        }
+      })
+      return winner
     }
   },
   methods: {
     moveTopPage () {
+      if (this.activePage.page === 'mode' || !confirm('トップページに戻りますか？')) {
+        return;
+      }
       this.page = 1
       this.playerIndex = 0
       this.toggleIsShowJob(false)
@@ -203,8 +319,9 @@ export default {
       this.resetJob()
     },
     nextPage () {
+      let confirmText = ''
       if (this.activePage.page === 'night-start') {
-        this.shuffleJob();
+        this.shuffleJob()
       } else if (this.activePage.page === 'night-turn') {
         if (!this.isShowJob) {
           alert('自分の役職を確認してください。')
@@ -214,10 +331,27 @@ export default {
           alert('占う人を選択してください。')
           return
         }
-        // 怪盗確認
+        if (this.playerList[this.playerIndex].job === 'thief' && !this.isChanged) {
+          alert('交換する人を選択してください。')
+          return
+        }
         if (this.playerList[this.playerIndex+1]) {
           this.toggleIsShowJob(false)
           this.playerIndex++
+          return
+        }
+      } else if (this.activePage.page === 'talk') {
+        confirmText = '話し合いを終了して投票しますか？'
+      } else if (this.activePage.page === 'vote') {
+        if (!this.executionPlayerIndexes.length) {
+          alert('追放する人を選択してください。')
+          return
+        }
+        confirmText = this.executionPlayerName + 'を追放します。よろしいですか。'
+      }
+
+      if (confirmText) {
+        if (!confirm(confirmText)) {
           return
         }
       }
@@ -227,13 +361,13 @@ export default {
       }
     },
     addPlayer () {
-      this.playerList.push({name: ''})
+      this.playerList.push({name: '', job:'', finalJob:'', isVote: false})
     },
     deletePlayer (index) {
       this.playerList.splice(index, 1)
     },
     getPlayerName (index) {
-      return this.playerList[index].name || ('プレイヤー' + (index+1))
+      return this.playerList[index] && this.playerList[index].name || ('プレイヤー' + (index+1))
     },
     getJobName (job) {
       if (job === 'werewolf') {
@@ -246,11 +380,12 @@ export default {
       return '村人'
     },
     shuffleJob () {
-      var shuffledIndexes = []
-      var min = 0
-      var max = this.playerList.length + 1
+      let shuffledIndexes = []
+      let min = 0
+      let max = this.playerList.length + 1
+      let ramdomIndex
       while (shuffledIndexes.length < 6) {
-        var ramdomIndex = Math.floor( Math.random() * (max + 1 - min) ) + min
+        ramdomIndex = Math.floor( Math.random() * (max + 1 - min) ) + min
         if (shuffledIndexes.indexOf(ramdomIndex) === -1) {
           shuffledIndexes.push(ramdomIndex)
         }
@@ -267,28 +402,37 @@ export default {
       this.playerList.forEach((player) => {
         player.job = ''
         player.finalJob = ''
+        player.isVote = false
       })
     },
     toggleIsShowJob (isShowJob) {
       this.isShowJob = isShowJob
     },
     fortunePlayer () {
-      if (!this.playerList[this.fortunePlayerIndex]) {
+      if (!this.playerList[this.fortunePlayerIndex] && this.fortunePlayerIndex != 'center') {
         alert('プレイヤーを選択してください')
         return
       }
-      this.isShowFotunedPlayer = true;
+
+      this.isShowFotunedPlayer = true
     },
     changePlayerJob () {
       if (!this.playerList[this.changePlayerIndex]) {
         alert('プレイヤーを選択してください')
         return
       }
-      var thiefIndex = this.playerIndex
+      let thiefIndex = this.playerIndex
       this.playerList[this.changePlayerIndex].finalJob = 'thief'
       this.playerList[thiefIndex].finalJob = this.playerList[this.changePlayerIndex].job
       this.isChanged = true
     },
+    getPlayerResult (job) {
+      if (job === 'werewolf') {
+        return this.winnerTeam === 'werewolf' ? true : false
+      } else {
+        return this.winnerTeam === 'villager' ? true : false
+      }
+    }
   }
 }
 </script>
@@ -355,6 +499,11 @@ body {
 
 .margin-top {
   margin-top: 30px;
+}
+
+.strong-text {
+  font-weight: bold;
+  color: #e8204e;
 }
 
 .footer {
